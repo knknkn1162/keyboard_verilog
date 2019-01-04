1c4c5d88a97c9a70e74fce113d403ed697cf171f
knknkn1162 <knknkn1162@gmail.com>
2019-01-04 19:24:23 +0900

    impl keyboard_negedge_detector module

---
+++ kb/keyboard_negedge_detector.v
@@ -0,0 +1,33 @@
+`ifndef _keyboard_negedge_detector
+`define _keyboard_negedge_detector
+
+`include "enable_gen.v"
+`include "posedge_detector.v"
+
+module keyboard_negedge_detector (
+  input wire clk, i_sclr, i_ps2_clk_n,
+  output wire o_edge_en
+);
+
+  wire s_sampling_en;
+
+  assign s_ps2_clk = ~i_ps2_clk_n;
+  localparam SAMPLING_BIT_SIZE = 5; // approx. 1.6 MHz
+
+  enable_gen #(SAMPLING_BIT_SIZE) enable_gen0(
+    .clk(clk),
+    .i_sclr(i_sclr),
+    .o_en(s_sampling_en)
+  );
+
+  posedge_detector clk_posedge_detector (
+    .clk(clk),
+    .i_sclr(i_sclr),
+    .i_en(s_sampling_en),
+    .i_dat(s_ps2_clk),
+    .o_posedge(o_edge_en)
+  );
+
+endmodule
+
+`endif
---
+++ kb/keyboard_negedge_detector_tb.v
@@ -0,0 +1,8 @@
+`include "testbench.v"
+`include "keyboard_negedge_detector.v"
+
+module keyboard_negedge_detector_tb;
+  initial begin
+    `alert_empty_tb;
+  end
+endmodule
