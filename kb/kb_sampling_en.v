36be1e957a155fc88c144ce6ea4b72062c1f498b
knknkn1162 <knknkn1162@gmail.com>
2019-01-06 12:55:33 +0900

    fix kb_sampling_en

--- kb/kb_sampling_en.v
+++ kb/kb_sampling_en.v
@@ -11,7 +11,7 @@ module kb_sampling_en (
 
   wire s_sampling_en;
 
-  localparam SAMPLING_BIT_SIZE = 5; // approx. 1.6 MHz
+  localparam SAMPLING_BIT_SIZE = 6; // approx. 0.8 MHz
 
   enable_gen #(SAMPLING_BIT_SIZE) enable_gen0(
     .clk(clk),
@@ -19,13 +19,16 @@ module kb_sampling_en (
     .o_en(s_sampling_en)
   );
 
-  posedge_detector clk_posedge_detector (
-    .clk(clk),
-    .i_sclr(i_sclr),
-    .i_en(s_sampling_en),
-    .i_dat(~i_ps2_clk_n),
-    .o_posedge(o_edge_en)
-  );
+  reg [5:0] r_en;
+  always @(posedge clk) begin
+    if (i_sclr) begin
+      r_en <= 6'b000000;
+    end else if (s_sampling_en) begin
+      r_en <= {r_en[4:0], i_ps2_clk_n};
+    end
+  end
+
+  assign o_edge_en = (r_en[5:0] == 6'b111000) & s_sampling_en;
 
 endmodule
 
