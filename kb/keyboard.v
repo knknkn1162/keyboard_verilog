cb820c3c3260e67faf99f7166aa2544d70c0386a
knknkn1162 <knknkn1162@gmail.com>
2019-01-06 10:48:55 +0900

    rename _data with _byte

--- kb/keyboard.v
+++ kb/keyboard.v
@@ -15,7 +15,8 @@ module keyboard (
 
   wire s_ps2_clk;
   wire s_edge_en;
-  wire [23:0] s_data;
+  wire [23:0] s_byte;
+  wire s_byte_en;
 
   keyboard_negedge_detector keyboard_negedge_detector0 (
     .clk(clk),
@@ -34,14 +35,15 @@ module keyboard (
     .i_sclr(i_sclr),
     .i_en(s_edge_en),
     .i_dat(i_ps2_dat),
-    .o_data(s_data[7:0])
+    .o_byte_en(s_byte_en),
+    .o_byte(s_byte[7:0])
   );
 
-  assign s_data[23:16] = s_cnt;
-  assign s_data[15:8] = 8'h00;
+  assign s_byte[23:16] = s_cnt;
+  assign s_byte[15:8] = 8'h00;
 
   hex_display hex_display0 (
-    .i_num(s_data),
+    .i_num(s_byte),
     .o_hex0(o_hex0),
     .o_hex1(o_hex1),
     .o_hex2(o_hex2),
--- kb/recv.v
+++ kb/recv.v
@@ -6,13 +6,14 @@
 
 module recv (
   input wire clk, i_sclr, i_en, i_dat,
-  output wire [7:0] o_data
+  output wire o_byte_en,
+  output wire [7:0] o_byte
 );
 
   localparam BIT_SIZE = 8;
-  wire [BIT_SIZE-1:0] s_data0, s_data1, s_data2;
+  wire [BIT_SIZE-1:0] s_byte0, s_byte1, s_byte2;
   wire s_parity0, s_parity1;
-  wire s_data_en;
+  wire s_byte_en;
   wire [3:0] s_stage, s_nextstage;
 
   flopr_en #(4) flopr_stage (
@@ -24,14 +25,14 @@ module recv (
   );
   assign s_nextstage = nextstage(s_stage, i_dat, s_parity1);
 
-  flopr_en #(BIT_SIZE) flopr_data0 (
+  flopr_en #(BIT_SIZE) flopr_byte0 (
     .clk(clk),
     .i_sclr(i_sclr),
     .i_en(i_en),
-    .i_a(s_data0),
-    .o_y(s_data1)
+    .i_a(s_byte0),
+    .o_y(s_byte1)
   );
-  assign s_data0 = (s_stage >=4'b0001 && s_stage <= 4'b1000) ? {i_dat, s_data1[BIT_SIZE-1:1]} : s_data1;
+  assign s_byte0 = (s_stage >=4'b0001 && s_stage <= 4'b1000) ? {i_dat, s_byte1[BIT_SIZE-1:1]} : s_byte1;
 
   bflopr_en bflopr_parity (
     .clk(clk),
@@ -42,16 +43,18 @@ module recv (
   );
   assign s_parity0 = (s_stage >= 4'b0001 && s_stage <= 4'b1000) ? s_parity1 + i_dat : 1'b1;
   
-  assign s_data_en = (s_stage == 4'b1010) ? 1'b1 : 1'b0;
-  flopr_en #(8) flopr_data1 (
+  assign s_byte_en = (s_stage == 4'b1010) ? 1'b1 : 1'b0;
+  flopr_en #(8) flopr_byte1 (
     .clk(clk),
     .i_sclr(i_sclr),
-    .i_en(s_data_en),
-    .i_a(s_data1),
-    .o_y(s_data2)
+    .i_en(s_byte_en),
+    .i_a(s_byte1),
+    .o_y(s_byte2)
   );
 
-  assign o_data = s_data2;
+  assign o_byte_en = s_byte_en;
+
+  assign o_byte = s_byte2;
 
   localparam START_BIT = 1'b0;
   localparam INIT_STATE = 4'b0000;
@@ -63,7 +66,7 @@ module recv (
     // start bit
     if (stage == 4'b0000) begin
       nextstage = (dat== START_BIT) ? 4'b0001 : INIT_STATE;
-    // data
+    // byte
     end else if (stage >= 4'b0001 && stage <= 4'b1000) begin
       nextstage = stage + 1'b1;
     // parity bit
